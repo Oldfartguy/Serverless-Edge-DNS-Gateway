385fda002bf8ed56e0319416ee670b9c38c81892
Oldfartguy <mail.dqt@protonmail.com>
2026-04-28 00:53:50 +0700

    Update primary DNS upstream URL

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,5 +1,5 @@
 // ==================== CONFIG ====================
-const UPSTREAM_PRIMARY = 'https://dns.opendns.com/dns-query';
+const UPSTREAM_PRIMARY = 'https://gppl2d20rh.cloudflare-gateway.com/dns-query';
 const UPSTREAM_FALLBACK = 'https://rhpcv957tj.cloudflare-gateway.com/dns-query';
 const UPSTREAM_GEO_BYPASS = 'https://dns.mullvad.net/dns-query'; // Re-resolve without ECS when geo-block returns loopback
 const UPSTREAM_TIMEOUT = 5000;
