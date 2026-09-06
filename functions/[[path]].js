af63243cec25e7ecd1f18266ae8ad4a94f1c5f48
Oldfartguy <mail.dqt@protonmail.com>
2026-09-07 01:55:13 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,5 +1,5 @@
 // ==================== CONFIG ====================
-const UPSTREAM_PRIMARY = 'https://dns.google/dns-query';
+const UPSTREAM_PRIMARY = 'https://gppl2d20rh.cloudflare-gateway.com/dns-query';
 const UPSTREAM_FALLBACK = 'https://iarmp87z8f.cloudflare-gateway.com/dns-query';
 const UPSTREAM_GEO_BYPASS = 'https://dns.quad9.net/dns-query'; // Re-resolve without ECS when geo-block returns loopback
 const UPSTREAM_TIMEOUT = 5000;
